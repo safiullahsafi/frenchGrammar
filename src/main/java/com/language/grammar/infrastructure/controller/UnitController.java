823235a231ee4d2b1f9bcd1681783269bdd3f851
safiullahsafi <safiullahsafi45@gmail.com>
2020-06-13 21:43:31 +0200

    bug fix

--- src/main/java/com/language/grammar/infrastructure/controller/UnitController.java
+++ src/main/java/com/language/grammar/infrastructure/controller/UnitController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
 public class UnitController {
     private UnitUsecase unitUsecase;
 
